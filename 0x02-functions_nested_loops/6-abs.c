15dd83841504c4f93e441ffb35de5cd7ef40f428
Esmail3576 <esmaeel3576@gmail.com>
2023-09-12 11:48:40 -0700

    add 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _abs - return absoulte value of a number
+ *
+ * @c: represent a number
+ *
+ * Return: Always 0.
+ *
+ */
+int _abs(int c);
+{
+	int abs;
+
+	if (c < 0)
+	{
+		abs = (-1 * abs);
+		return (abs);
+	}
+	else
+	{
+		abs = (1 * abs);
+		return (abs);
+	}
+}
