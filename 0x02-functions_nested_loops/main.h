196b8787d2bfcf173cd23aa44f4ab93b62321a51
Esmail3576 <esmaeel3576@gmail.com>
2023-09-12 06:15:24 -0700

    update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,19 @@
 #include <stdio.h>
+#include "_putchar.c"
 void print_alphabet(void);
+
+/**
+ * print_alphabet - print small letters
+ *
+ * Retrutn: 0.
+ *
+ */
+void print_alphabet(void)
+{
+	char ch[] = "abcdefghijklmnopqrstuvxwz";
+	int i;
+
+	for (i = 0; i < 27; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+}
