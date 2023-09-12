987a8919be3d3d574dc9be39b286b477b6269365
Esmail3576 <esmaeel3576@gmail.com>
2023-09-12 06:21:36 -0700

    add 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,28 +2,11 @@
 /**
  * main - Entry point
  *
- * Retrun: Always 0
+ * Retrun: Always 0.
  *
  */
-void print_alphabet(void);
-
 int main(void)
 {
 	print_alphabet();
 	return (0);
-}
-/**
- * print_alphabet - print alphabet small letter
- *
- * Return: Always 0
- *
- */
-void print_alphabet(void)
-{
-	char ch[] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 27; i++)
-		putchar(ch[i]);
-	putchar('\n');
 }
