be59172f5593dc7307f6b82f912234faf985671f
Esmail3576 <esmaeel3576@gmail.com>
2023-09-12 18:27:42 -0700

    add 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,22 +15,16 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 7; j++)
 		{
-			_putchar('0' + i);
-			_putchar('0' + i);
-			_putchar(':');
-			_putchar('0' + j);
 			for (h = 0; h < 10; h++)
 			{
+				_putchar('0' + i);
+				_putchar('0' + i);
+				_putchar(':');
+				_putchar('0' + j);
+				_putchar('0' + h);
+				_putchar('\n');
 				if (j == 6)
-				{
-					_putchar('0' + h);
-					_putchar('\n');
-					h = 19;
-				}
-				else
-				{
-					_putchar('0' + h);
-				}
+					h = 11;
 			}
 		}
 	}
