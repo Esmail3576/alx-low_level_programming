702e44494cbe780e960683fd9de6c9cbabd460e2
Esmail3576 <esmaeel3576@gmail.com>
2023-09-12 18:52:56 -0700

    add 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,22 +10,29 @@ void jack_bauer(void)
 	int i;
 	int j;
 	int h;
+	int t;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 7; j++)
+		for (t = 0; t < 10; t++)
 		{
-			for (h = 0; h < 10; h++)
+			h
+			for (j = 0; j < 7; j++)
 			{
-				_putchar('0' + i);
-				_putchar('0' + i);
-				_putchar(':');
-				_putchar('0' + j);
-				_putchar('0' + h);
-				_putchar('\n');
-				if (j == 6)
-					h = 11;
+				for (h = 0; h < 10; h++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + t);
+					_putchar(':');
+					_putchar('0' + j);
+					_putchar('0' + h);
+					_putchar('\n');
+					if (j == 5 && h == 9)
+						h = 11;
+				}
 			}
 		}
+		if (i == 2 && t == 3)
+			i = 4;
 	}
 }
