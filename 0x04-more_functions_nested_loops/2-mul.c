8104423a065dcd0f53dd871d419684ef5d4f8a6f
Esmail3576 <esmaeel3576@gmail.com>
2023-09-14 18:55:28 -0700

    add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiply tow integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiply tow intgers
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
