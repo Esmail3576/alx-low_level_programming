8e88e30a41df4069a597ecff3509f1ad0145542c
Esmail3576 <esmaeel3576@gmail.com>
2023-09-15 14:18:41 -0700

    add 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - draw a triangle line in terminal
+ *
+ * @size: size of a triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar(' ');
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
