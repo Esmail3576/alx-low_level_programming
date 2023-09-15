824402afbefd507291f11d7945de83238567e7e6
Esmail3576 <esmaeel3576@gmail.com>
2023-09-15 11:18:51 -0700

    add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check upper case letter
+ *
+ * @c: integer value of char
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	 if (c >= 65 && c < 91)
+		 return (1);
+	 else
+		 return (0);
+}
