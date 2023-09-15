18e56feca7b0c3dde72b5611f6b0f414db29fd59
Esmail3576 <esmaeel3576@gmail.com>
2023-09-15 12:11:19 -0700

    add 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - print form 0 to 9 without 2,4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char c[] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (c[i] != 2 && c[i] != 4)
+			_putchar(c[i]);
+	}
+	_putchar('\n');
+}
