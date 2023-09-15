84cbf1984780c220dcf39026a0a923cc9e354ef5
Esmail3576 <esmaeel3576@gmail.com>
2023-09-14 19:02:08 -0700

    add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 - 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char numbers[] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10)
+		_putchar(numbers[i]);
+	_putchar('\n');
+}
