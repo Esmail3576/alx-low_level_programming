6884aab8480f76e7b95284ef49c13108392d03cf
Esmail3576 <esmaeel3576@gmail.com>
2023-09-18 14:26:18 -0700

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ *
+ * @c: pointer to char
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+		i++;
+	Return (i):
+}
