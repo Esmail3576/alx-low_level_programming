a14eb1eebdde2197cfabb0eb4a94f8912c9bd54b
Esmail3576 <esmaeel3576@gmail.com>
2023-09-18 19:21:01 -0700

    add 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 /**
  * _strlen - returns length of string
  *
- * @c: pointer to char
+ * @s: pointer to char
  *
  * Return: length of string
  */
 int _strlen(char *s)
 {
-	int i = 0;
+	int i = 1;
 
 	while (*s != '\0')
 		i++;
