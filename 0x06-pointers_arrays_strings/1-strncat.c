66bf39bd86700382f271e7668c161649f6c02147
Esmail3576 <esmaeel3576@gmail.com>
2023-09-21 17:56:40 -0700

    add 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,11 +12,19 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int length = 0;
 	int j = 0;
+	char *str;
 
 	while (dest[length] != '\0')
 		length++;
 	while (j < n)
 		dest[length + j] = src[j];
+	length = length + j;
 	dest[length + j] = src[j];
-	return (dest);
+	*str = dest;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		str[length] = src[j];
+		length++;
+	}
+	return (str);
 }
