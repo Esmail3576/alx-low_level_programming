5fc720e5d744f4d63c8cb0e53b981a8ae91a4f0c
Esmail3576 <esmaeel3576@gmail.com>
2023-09-21 17:22:45 -0700

    add 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,26 @@
+
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @dest: string with lowercase
+ *
+ * Return: uppercase string
+ */
+char *string_toupper(char *dest)
+{
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+	{
+		for (j = 0; j < 27; j++)
+		{
+			if (dest[i] == lower[j])
+				dest[i] = upper[j];
+		}
+	}
+	return (dest);
+}
