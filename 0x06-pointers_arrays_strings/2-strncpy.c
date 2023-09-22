db43e5d2e002fe82e6f1d6621553928afcbd4991
Esmail3576 <esmaeel3576@gmail.com>
2023-09-21 18:50:06 -0700

    add 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,15 @@ char *_strncpy(char *dest, char *src, int n)
 	int len = 0;
 	int reset = 0;
 
+	while (src[len] != '\0' && len < n)
+	{
+		dest[len] = src[len];
+		len++;
+	}
 	while (len < n)
 	{
-		if (src[len] == '\0')
-			reset = 0;
-		dest[len] = src[reset];
+		dest[len] = '\0';
 		len++;
-		reset++;
 	}
 	return (dest);
 }
