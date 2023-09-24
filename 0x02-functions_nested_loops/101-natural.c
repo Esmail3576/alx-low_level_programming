7925c5c10b291138767faea01aa0a1922de38084
Esmail3576 <esmaeel3576@gmail.com>
2023-09-24 03:46:10 -0700

    add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - computes multiply of 3 & 5
+ *
+ * Return: sucess
+ */
+int main(void)
+{
+	int i;
+	int r = 0;
+
+	for (i  = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			r = r + i;
+		else if (i % 5 == 0)
+			r = r + i;
+		else if (i % 3 == 0)
+			r = r + i;
+	}
+	printf("%d", r);
+	return (0);
+}
