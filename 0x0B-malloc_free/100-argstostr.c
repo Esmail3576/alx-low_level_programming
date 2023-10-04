11b7506bac4b8760382aa17985f8031be1b14af4
Esmail3576 <esmaeel3576@gmail.com>
2023-10-04 16:09:38 -0700

    advanc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: integer
+ * @av: 2d array
+ * Retrun: pointer
+ */
+char *argstostr(int ac, char **av)
+{
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * strtow - plits a string into words.
+ * @str: string
+ * Return: 2d array
+ */
+char **strtow(char *str)
+{
+}
