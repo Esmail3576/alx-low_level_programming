c032ee9e0834ad6975df616e311b293420edbe9b
Esmail3576 <esmaeel3576@gmail.com>
2023-10-07 11:45:08 -0700

    add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * main - print arg name
- * Retuen: 
+ * Retuen: 0 
  */
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s", argv[0]);
 	return (0);
