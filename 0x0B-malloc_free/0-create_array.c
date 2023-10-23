3f83ac4649780f0dafeff1ee7f3a53316512b3ff
Esmail3576 <esmaeel3576@gmail.com>
2023-10-23 13:30:49 -0700

    update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,8 +10,9 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	unsigned int i;
 
-	array = malloc(size * 5);
+	array = malloc(size * size);
 	if (size == 0)
 	{
 		return (NULL);
@@ -22,7 +23,8 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		*array = c;
+		for (i = 0; i <= size; i++)
+			array[i] = c;
 		return (array);
 	}
 }
